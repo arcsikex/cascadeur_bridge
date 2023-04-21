f1b5c7333c9133fc4cd47cd4b75ef5d55ecac7fd
arcsikex <arcsikex@hotmail.com>
2023-04-21 19:16:50 +0200

    csc fbx export settings example

--- csc_files/externals/temp_exporter.py
+++ csc_files/externals/temp_exporter.py
@@ -49,3 +49,23 @@ def run(scene):
     fbx_scene_loader.export_all_objects(export_path)
     scene.info(f"File exported to {export_path}")
     send_file_path(export_path)
+
+
+def save_func(file_name):
+    mp = csc.app.get_application()
+    settings = csc.fbx.FbxSettings()
+    settings.mode = csc.fbx.FbxSettingsMode.Ascii
+    settings.export_selected_interval = True
+    settings.apply_euler_filter = False
+    settings.up_axis = csc.fbx.FbxSettingsAxis.Y
+
+    scene_pr = mp.get_scene_manager().current_scene()
+    fbx_scene_loader = (
+        csc.app.get_application()
+        .get_tools_manager()
+        .get_tool("FbxSceneLoader")
+        .get_fbx_loader(scene_pr)
+    )
+    fbx_scene_loader.set_settings(settings)
+    fbx_scene_loader.export_joints(file_name)
+    mp.get_action_manager().call_action("Scene.Undo")
