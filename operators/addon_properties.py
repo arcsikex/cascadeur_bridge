fb2917ee493578708fb591d0e2a638ca8d8dbe6f
Áron Németh <anemeth@dmhlab.hu>
2023-07-20 15:10:09 +0200

    read all fbx export/import settings from config file

--- operators/addon_properties.py
+++ operators/addon_properties.py
@@ -26,108 +26,214 @@ class CBB_PG_fbx_settings(bpy.types.PropertyGroup):
     cbb_csc_apply_euler_filter: bpy.props.BoolProperty(
         name="Apply Euler Filter",
         description="Automatically set objects' rotations to lowes possible values",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_csc_apply_euler_filter",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_csc_up_axis: bpy.props.EnumProperty(
         items=generate_items(["Y", "Z"]),
         name="Up Axis",
         description="Up Axis when exporting from Cascadeur",
-        default="Y",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_csc_up_axis",
+            str,
+            fallback="Y",
+            config=config,
+        ),
     )
 
     cbb_csc_bake_animation: bpy.props.BoolProperty(
         name="Bake animation",
         description="Key all frames when exporting from Cascadeur",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_csc_bake_animation",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     # Blender Import settings
     cbb_import_global_scale: bpy.props.FloatProperty(
-        name="Global Scale", description="Scale", default=1.0, min=0.001, max=1000
+        name="Global Scale",
+        description="Scale",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_global_scale",
+            float,
+            fallback=1.0,
+            config=config,
+        ),
+        min=0.001,
+        max=1000,
     )
 
     cbb_import_apply_transform: bpy.props.BoolProperty(
         name="Apply Transform",
         description="Bake space transform into object data. EXPERIMENTAL!",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_apply_transform",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_import_manual_orientation: bpy.props.BoolProperty(
         name="Use Manual Orientation",
         description="Specify orientation and scale, instead of using embedded data in FBX file",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_manual_orientation",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_import_axis_forward: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Forward",
         description="Forward Axis",
-        default="-Z",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_axis_forward",
+            str,
+            fallback="-Z",
+            config=config,
+        ),
     )
 
     cbb_import_axis_up: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Up",
         description="Forward Up",
-        default="Y",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_axis_up",
+            str,
+            fallback="Y",
+            config=config,
+        ),
     )
 
     cbb_import_use_anim: bpy.props.BoolProperty(
         name="Import Animation",
         description="Import FBX animation",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_use_anim",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     cbb_import_anim_offset: bpy.props.FloatProperty(
         name="Animation Offset",
         description=" Offset to apply to animation during import, in frames",
-        default=1.0,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_anim_offset",
+            float,
+            fallback=1.0,
+            config=config,
+        ),
     )
 
     cbb_import_ignore_leaf_bones: bpy.props.BoolProperty(
         name="Ignore Leaf Bones",
         description="Ignore the last bone at the end of each chain",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_ignore_leaf_bones",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     cbb_import_force_connect_children: bpy.props.BoolProperty(
         name="Force Connect Children",
         description="Force connection of children bones to their parent",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_force_connect_children",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_import_automatic_bone_orientation: bpy.props.BoolProperty(
         name="Automatic Bone Orientation",
         description="Try to align the major bone axis with the bone children",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_automatic_bone_orientation",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_import_primary_bone_axis: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Primary Bone Axis",
         description="",
-        default="Y",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_primary_bone_axis",
+            str,
+            fallback="Y",
+            config=config,
+        ),
     )
 
     cbb_import_secondary_bone_axis: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Secondary Bone Axis",
         description="",
-        default="X",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_secondary_bone_axis",
+            str,
+            fallback="X",
+            config=config,
+        ),
     )
 
     cbb_import_use_prepost_rot: bpy.props.BoolProperty(
         name="Use Pre/Post Rotation",
         description="Use pre/post rotation from FBX transform",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_import_use_prepost_rot",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     # Blender Export settings
     cbb_export_use_selection: bpy.props.BoolProperty(
         name="Selected Objects",
         description="Export selected and visible objects only",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_use_selection",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     cbb_export_object_types: bpy.props.EnumProperty(
@@ -156,71 +262,141 @@ class CBB_PG_fbx_settings(bpy.types.PropertyGroup):
     )
 
     cbb_export_global_scale: bpy.props.FloatProperty(
-        name="Global Scale", description="Scale", default=1.0, min=0.001, max=1000
+        name="Global Scale",
+        description="Scale",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_global_scale",
+            float,
+            fallback=1.0,
+            config=config,
+        ),
+        min=0.001,
+        max=1000,
     )
 
     cbb_export_axis_forward: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Forward",
         description="Forward Axis",
-        default="-Z",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_axis_forward",
+            str,
+            fallback="-Z",
+            config=config,
+        ),
     )
 
     cbb_export_axis_up: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Up",
         description="Forward Up",
-        default="Y",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_axis_up",
+            str,
+            fallback="Y",
+            config=config,
+        ),
     )
 
     cbb_export_apply_transform: bpy.props.BoolProperty(
         name="Apply Transform",
         description="Bake space transform into object data. EXPERIMENTAL!",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_apply_transform",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_export_primary_bone_axis: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Primary Bone Axis",
         description="",
-        default="Y",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_primary_bone_axis",
+            str,
+            fallback="Y",
+            config=config,
+        ),
     )
 
     cbb_export_secondary_bone_axis: bpy.props.EnumProperty(
         items=generate_items(["X", "Y", "Z", "-X", "-Y", "-Z"]),
         name="Secondary Bone Axis",
         description="",
-        default="X",
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_secondary_bone_axis",
+            str,
+            fallback="X",
+            config=config,
+        ),
     )
 
     cbb_export_deform_only: bpy.props.BoolProperty(
         name="Only Deform Bones",
         description="Only write deforming bones",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_deform_only",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     cbb_export_leaf_bones: bpy.props.BoolProperty(
         name="Add Leaf Bones",
         description="Append a final bone to the end of each chain to specify last bone length",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_leaf_bones",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_export_bake_anim: bpy.props.BoolProperty(
         name="Baked Animation",
         description="Export baked keyframe animation",
-        default=True,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_bake_anim",
+            bool,
+            fallback=True,
+            config=config,
+        ),
     )
 
     cbb_export_use_nla_strips: bpy.props.BoolProperty(
         name="NLA Strips",
         description="Export each non-muted NLA strip as a separated FBX’s AnimStack, if any, instead of global scene animation",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_use_nla_strips",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
     cbb_export_use_all_actions: bpy.props.BoolProperty(
         name="All Actions",
         description="Export each action as a separated FBX’s AnimStack, instead of global scene animation",
-        default=False,
+        default=config_handling.get_config_parameter(
+            "FBX Settings",
+            "cbb_export_use_all_actions",
+            bool,
+            fallback=False,
+            config=config,
+        ),
     )
 
 
@@ -249,7 +425,7 @@ class CBB_OT_save_fbx_settings(bpy.types.Operator):
     """Save fbx import and export settings for Cascadeur and Blender"""
 
     bl_idname = "cbb.save_fbx_settings"
-    bl_label = "Save Settings"
+    bl_label = "Save Settings for CSC Bridge"
 
     def execute(self, context):
         config_handling.save_fbx_settings()
