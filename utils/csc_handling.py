2ad83fd249aa89b9a5b839637a38b2b6d80924c3
Áron Németh <anemeth@dmhlab.hu>
2023-11-30 16:39:13 +0100

    add docstrings to csc_handling

--- utils/csc_handling.py
+++ utils/csc_handling.py
@@ -8,6 +8,11 @@ from ..addon_info import PACKAGE_NAME
 
 
 def get_default_csc_exe_path() -> str:
+    """
+    Returns the default Cascadeur executable path based on the operating system.
+
+    :return str: Default Cascadeur executable path, or an empty string if not found.
+    """
     csc_path = {
         "Windows": r"C:\Program Files\Cascadeur\cascadeur.exe",
         "Linux": r"/opt/cascadeur/cascadeur",
@@ -20,12 +25,22 @@ def get_default_csc_exe_path() -> str:
 class CascadeurHandler:
     @property
     def csc_exe_path_addon_preference(self) -> str:
+        """
+        Get the set Cascadeur executable path from the addon's preferences.
+
+        :return str: Cascadeur executable path stored in the addon's preferences.
+        """
         preferences = bpy.context.preferences
         addon_prefs = preferences.addons[PACKAGE_NAME].preferences
         return addon_prefs.csc_exe_path
 
     @property
     def csc_dir(self) -> str:
+        """
+        Get the root directory of Cascadeur installation.
+
+        :return str: Directory path as a string.
+        """
         if self.is_csc_exe_path_valid:
             return (
                 self.csc_exe_path_addon_preference
@@ -35,11 +50,21 @@ class CascadeurHandler:
 
     @property
     def is_csc_exe_path_valid(self) -> bool:
+        """
+        Check if the Cascadeur executable path is valid.
+
+        :return bool: True if file exists, False otherwise.
+        """
         csc_path = self.csc_exe_path_addon_preference
         return True if csc_path and file_handling.file_exists(csc_path) else False
 
     @property
     def commands_path(self) -> str:
+        """
+        Get the path to the Cascadeur commands directory.
+
+        :return str: Directory path as a string.
+        """
         resources_dir = (
             os.path.join(self.csc_dir, "Contents", "MacOS", "resources")
             if platform.system() == "Darwin"
@@ -48,8 +73,19 @@ class CascadeurHandler:
         return os.path.join(resources_dir, "scripts", "python", "commands")
 
     def start_cascadeur(self) -> None:
+        """
+        Start Cascadeur using the specified executable path.
+
+        :return: None
+        """
         csc_path = self.csc_exe_path_addon_preference
         subprocess.Popen([csc_path])
 
     def execute_csc_command(self, command: str) -> None:
+        """
+        Execute a Cascadeur command using the specified executable path.
+
+        :param command str: Cascadeur command to execute.
+        :return: None
+        """
         subprocess.Popen([self.csc_exe_path_addon_preference, "--run-script", command])
